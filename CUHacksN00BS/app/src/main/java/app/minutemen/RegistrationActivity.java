5e72e04155a9423cbcf6b58920a37556497d429c
Vincenth421 <vincenthwang421@gmail.com>
2016-06-12 05:00:04 -0700

    asdfasdfsad

--- CUHacksN00BS/app/src/main/java/app/minutemen/RegistrationActivity.java
+++ CUHacksN00BS/app/src/main/java/app/minutemen/RegistrationActivity.java
@@ -34,15 +34,6 @@ public class RegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         //An object for each component.
         name = Utils.getComponent(R.id.editText, this);
 
